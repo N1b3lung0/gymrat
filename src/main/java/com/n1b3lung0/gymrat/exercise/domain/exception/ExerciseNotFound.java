95b0f140ae22e3882d1433f0da66cf60c7be779b
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2024-10-24 17:08:46 +0200

    #17 Add custom exceptions for Exercise Entity

---
+++ src/main/java/com/n1b3lung0/gymrat/exercise/domain/exception/ExerciseNotFound.java
@@ -0,0 +1,15 @@
+package com.n1b3lung0.gymrat.exercise.domain.exception;
+
+import com.n1b3lung0.gymrat.common.exception.domain.NotFound;
+
+import java.io.Serial;
+
+public final class ExerciseNotFound extends NotFound {
+
+    @Serial
+    private static final long serialVersionUID = -1L;
+
+    public ExerciseNotFound(String id) {
+        super("An exercise with ID '%s' could not be found", id);
+    }
+}
