1ffb9add511d8e6acfd7a2d5d2c32e6961244395
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2024-10-30 16:16:57 +0100

    #47 Perform DeleteSeries operation

--- src/main/java/com/n1b3lung0/gymrat/series/application/delete/SeriesDeleter.java
+++ src/main/java/com/n1b3lung0/gymrat/series/application/delete/SeriesDeleter.java
@@ -1,11 +1,26 @@
 package com.n1b3lung0.gymrat.series.application.delete;
 
+import com.n1b3lung0.gymrat.common.log.application.LogConstants;
+import com.n1b3lung0.gymrat.series.application.find.SeriesFinder;
+import com.n1b3lung0.gymrat.series.domain.Series;
+import com.n1b3lung0.gymrat.series.domain.SeriesRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class SeriesDeleter {
+    private final SeriesFinder finder;
+    private final SeriesRepository repository;
+
+    @Transactional
+    public void delete(String id) {
+        Series series = finder.findById(id);
+        Series deleted = series.delete(series, "n1b3lung0");
+        repository.save(deleted);
+        log.debug(LogConstants.DELETED, "SERIES", deleted);
+    }
 }
