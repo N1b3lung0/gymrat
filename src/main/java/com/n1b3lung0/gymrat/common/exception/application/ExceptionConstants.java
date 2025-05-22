cb06e1a22f3eb11f1615dada2e5779d4ff90ff10
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2025-05-22 17:15:26 +0200

    Update ExceptionConstants

--- src/main/java/com/n1b3lung0/gymrat/common/exception/application/ExceptionConstants.java
+++ src/main/java/com/n1b3lung0/gymrat/common/exception/application/ExceptionConstants.java
@@ -37,9 +37,26 @@ public final class ExceptionConstants {
 
     // USER
     public static final String USER_NOT_FOUND = "User with %s '%s' could not be found";
+    public static final String USER_NOT_VALID = "User '%s' is not valid. Reason: %s";
+    public static final String USERNAME_REPEATED = "A user with username %s has already been created";
+    public static final String GROUP_ROLES_AND_PERMISSIONS_MISMATCH = "The number of group roles and groups with permissions must be the same";
+    public static final String USERNAME_LENGTH_NOT_VALID = "Username must be between {min} and {max} characters long";
+    public static final String USER_DELETE_NOT_ALLOWED = "User delete not allowed. Reason: %s";
+    public static final String DEFAULT_USER_CANNOT_BE_REMOVED = "Default user cannot be removed";
+    public static final String USER_UPDATE_NOT_ALLOWED = "User update not allowed. Reason: %s";
+    public static final String DEFAULT_USER_CANNOT_BE_UPDATED = "Default user cannot be updated";
 
     // USER PASSWORD
     public static final String USER_PASSWORD_REQUIRED = "User password is required";
     public static final String USER_PASSWORD_NOT_VALID = "User password '%s' is not a valid value. Reason: %s";
     public static final String PASSWORD_DEFAULT_REASON = "Password must contain between 8 and 20 characters with no whitespaces. It must contain at least one digit, one uppercase letter, one lowercase letter and one special character (!@#$%&*()-+=^)";
+    public static final String USER_PASSWORD_UPDATE_NOT_ALLOWED = "User password update not allowed. Reason: %s";
+    public static final String WRONG_OLD_PASSWORD = "Old password is wrong";
+    public static final String WRONG_REPEATED_PASSWORD = "New password and repeated password don't match";
+
+    // EMAIL
+    public static final String EMAIL_REQUIRED = "Email is required";
+    public static final String EMAIL_NOT_VALID = "Email '%s' is not a valid value. Reason: %s";
+    public static final String EMAIL_DEFAULT_REASON = "Email must contain any numbers, letters or RFC 5322 allowed characters followed by an @ symbol and any number of domain names separated by dots (e.g.: 'some.valid_name@thirdlevel.secondlevel.firstlevel')";
+
 }
