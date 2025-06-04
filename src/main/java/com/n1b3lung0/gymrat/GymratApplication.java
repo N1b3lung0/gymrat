6a4409327f28479e467274356294b8f293821be1
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2025-06-04 12:24:53 +0200

    Add Bean in main to encode passwords using bcrypt

--- src/main/java/com/n1b3lung0/gymrat/GymratApplication.java
+++ src/main/java/com/n1b3lung0/gymrat/GymratApplication.java
@@ -2,6 +2,10 @@ package com.n1b3lung0.gymrat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.security.SecureRandom;
 
 @SpringBootApplication
 public class GymratApplication {
@@ -10,4 +14,8 @@ public class GymratApplication {
 		SpringApplication.run(GymratApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(10, new SecureRandom());
+	}
 }
