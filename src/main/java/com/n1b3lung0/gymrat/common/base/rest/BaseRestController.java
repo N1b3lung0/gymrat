f634bb598e2182f52d73391bd08bd0bc2fd05ca6
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2025-06-04 12:24:21 +0200

    Delete name path and add email path

--- src/main/java/com/n1b3lung0/gymrat/common/base/rest/BaseRestController.java
+++ src/main/java/com/n1b3lung0/gymrat/common/base/rest/BaseRestController.java
@@ -3,5 +3,5 @@ package com.n1b3lung0.gymrat.common.base.rest;
 public abstract class BaseRestController {
 
     protected static final String ID_PATH = "/{id}";
-    protected static final String NAME_PATH = "/{name}";
+    protected static final String EMAIL = "/{email}";
 }
